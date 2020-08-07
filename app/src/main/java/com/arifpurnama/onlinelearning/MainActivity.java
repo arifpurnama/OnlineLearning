0d191a2f81270d507d95b5c77389bb872fd9e83f
purnamaarif <git.arifpurnama@gmail.com>
2020-08-07 16:54:30 +0700

    Setting Retrofit

--- app/src/main/java/com/arifpurnama/onlinelearning/MainActivity.java
+++ app/src/main/java/com/arifpurnama/onlinelearning/MainActivity.java
@@ -3,8 +3,21 @@ package com.arifpurnama.onlinelearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.arifpurnama.onlinelearning.model.Category;
+import com.arifpurnama.onlinelearning.retrofit.ApiInterface;
+import com.arifpurnama.onlinelearning.retrofit.RetrofitClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
         //setup retrofit for network call fething data
 
+        apiInterface = RetrofitClient.getRetrofitClient().create(ApiInterface.class);
+        Call<List<Category>> call = apiInterface.getAllCategory();
+        call.enqueue(new Callback<List<Category>>() {
+            @Override
+            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
+                List<Category> categoryList = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "No Response from server", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //lets run this app and check server is responding or not
+        
     }
 }
--- app/src/main/java/com/arifpurnama/onlinelearning/retrofit/ApiInterface.java
+++ app/src/main/java/com/arifpurnama/onlinelearning/retrofit/ApiInterface.java
@@ -1,4 +1,17 @@
 package com.arifpurnama.onlinelearning.retrofit;
 
+import com.arifpurnama.onlinelearning.model.Category;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiInterface {
+    @GET("course.json")
+    Call<List<Category>> getAllCategory();
+
+    // we need to make model class for our data
+    // first have a look on json structure.
+
 }
